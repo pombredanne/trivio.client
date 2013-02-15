80f5bb4a53df91768cb007180c52cfef1aadb3e5
Scott Robertson <srobertson@codeit.com>
2013-02-15 10:17:02 -0800

    added nose to requirements and wrecker file for CI

---
+++ tests/test_cmnd_line.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from triv.io import client
+
+class TestCommandLine(TestCase):
+  
+  def test_failure(self):
+    assert False
+    
+  def test_success(self):
+    return None
