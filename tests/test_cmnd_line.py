fae840f3ba5a256d5959e2636b452c859f12373a
Scott Robertson <srobertson@codeit.com>
2013-02-15 11:15:04 -0800

    remove failure

--- tests/test_cmnd_line.py
+++ tests/test_cmnd_line.py
@@ -3,9 +3,6 @@ from unittest import TestCase
 from triv.io import client
 
 class TestCommandLine(TestCase):
-  
-  def test_failure(self):
-    assert False
-    
+      
   def test_success(self):
     return None
