9035c7e93d37889d40fe7b67b75b0d18fd202c54
Scott Robertson <srobertson@codeit.com>
2013-02-17 09:54:51 -0800

    test create repository by name

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,7 @@ from StringIO import StringIO
 from unittest import TestCase
 from nose.tools import eq_
 from httpretty import httprettified
+from mock import Mock
 
 from triv.io import client as cli
 import helper
@@ -21,19 +22,47 @@ class TestCli(object):
     assert msg in self.output.getvalue(), 'stdout did not contain\n"{}"'.format(msg)
     
   def setUp(self):
+    self.saved_cwd = os.getcwd()
     self.output = StringIO()
     self.saved_stdout = sys.stdout
     sys.stdout = self.output
-    self.cookie_dir = mkdtemp()
+    self.temp_dir = mkdtemp()
+    self.cookie_dir = self.temp_dir#os.path.join(self.temp_dir, 'cookies')
     self.conn = helper.client(self.cookie_dir)
+    os.chdir(self.temp_dir)
 
   def tearDown(self):
     self.output.close()
     sys.stdout = self.saved_stdout
-    shutil.rmtree(self.cookie_dir)
+    os.chdir(self.saved_cwd)
+    shutil.rmtree(self.temp_dir)
   
   @httprettified  
   def test_login_exlpains_git_usage(self):
     helper.github_login_flow()
     cli.login_cmd(self.conn)
     self.contains("Triv.io uses github for authentication.")
+
+
+
+  def test_create_new_project(self):
+    session = Mock()
+    
+    session.create_project.return_value = '{}'    
+    cli.create_cmd(session, "acceptance")
+    
+    self.contains("Creating project directory")
+    self.contains("Creating repositories")
+    self.contains("Creating trivio project")
+    
+    assert session.create_project.called
+    assert os.path.isdir('acceptance') # command should create an directory
+    assert os.path.isfile('acceptance/.trivio.project')
+    assert os.path.isfile('acceptance/.git')
+    #assert os.path.path.isfile('acceptance/README')
+    
+    
+    
+    
+    
+    
